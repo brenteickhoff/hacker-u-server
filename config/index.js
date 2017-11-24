883e3ecbd599f1ee0be13422de980cc7703d3482
Brent Eickhoff <dev@eickhoffs.net>
2017-11-24 01:19:06 -0800

    Implement schema

---
+++ config/index.js
@@ -0,0 +1,13 @@
+// TODO: Do this the right way, move to process.env?
+
+export const server = {
+  port: 5000
+};
+
+export const db = {
+  name: 'hacker_u',
+  user: 'root',
+  password: '',
+  host: 'localhost',
+  dialect: 'mysql'
+};
